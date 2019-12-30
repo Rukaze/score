a170645425728f808af23a92cef7dd659970753c
EC2 Default User <ec2-user@ip-172-31-36-119.ap-northeast-1.compute.internal>
2019-12-30 04:39:28 +0000

    ok

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,8 +1,5 @@
 import Vue from 'vue/dist/vue.esm';
 import BootstrapVue from 'bootstrap-vue'
-import App from '../app.vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 
 
