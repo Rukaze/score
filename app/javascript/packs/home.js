19140cbb57598a254866cf56582e2038b7cb9142
EC2 Default User <ec2-user@ip-172-31-36-119.ap-northeast-1.compute.internal>
2019-12-30 03:00:54 +0000

    pro

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,5 +1,8 @@
 import Vue from 'vue/dist/vue.esm';
-import BootstrapVue from 'bootstrap-vue';
+import BootstrapVue from 'bootstrap-vue'
+import App from '../app.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 
 
